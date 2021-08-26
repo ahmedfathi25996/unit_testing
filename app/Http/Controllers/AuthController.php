c20c8a129e556a301b626a92e97833965e0b3de0
ahmed fathy <ahmedfathimamdouh25996@gmail.com>
2021-08-26 20:51:05 +0200

    qsq

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends BaseController
 
     function login(Request $request)
     {
-        
+        dd("hre");
         $data = $request->all();
         $request->validate([
            "email" => "required",
